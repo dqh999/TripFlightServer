54f198b309865eb391bd2b48d497a540931d186e
dqh999 <dqhit999@gmail.com>
2025-01-06 16:02:21 +0700

    enable cors

--- src/main/java/com/railgo/infrastructure/config/AppConfig.java
+++ src/main/java/com/railgo/infrastructure/config/AppConfig.java
@@ -10,22 +10,22 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class AppConfig {
-    @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-
-        config.addAllowedOrigin("http://localhost:8080");
-        config.addAllowedOrigin("http://api.railgo.dqhdev.io.vn");
-
-
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        config.addExposedHeader("Access-Control-Allow-Origin");
-        source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return bean;
-    }
+////    @Bean
+////    public FilterRegistrationBean<CorsFilter> corsFilter() {
+////        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+////        CorsConfiguration config = new CorsConfiguration();
+////        config.setAllowCredentials(true);
+////
+////        config.addAllowedOrigin("http://localhost:8080");
+////        config.addAllowedOrigin("http://api.railgo.dqhdev.io.vn");
+////
+////
+////        config.addAllowedHeader("*");
+////        config.addAllowedMethod("*");
+////        config.addExposedHeader("Access-Control-Allow-Origin");
+////        source.registerCorsConfiguration("/**", config);
+////        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+////        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+////        return bean;
+////    }
 }
